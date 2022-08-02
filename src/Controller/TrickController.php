d237e7d46ff132463e508f06b08d013581df7c2f
PalomaAlma <palomavacheron@gmail.com>
2022-08-02 16:24:10 +0200

    Fix trick delete

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -198,9 +198,10 @@ class TrickController extends AbstractController
      */
     public function delete(Request $request, Trick $trick, TrickRepository $trickRepository): Response
     {
+        $delete = 'delete'.$trick->getId();
         if ($trick->getAuthor() === $this->getUser())
         {
-            if ($this->isCsrfTokenValid('delete'.$trick->getId(), $request->request->get('_token'))) {
+            if ($this->isCsrfTokenValid($delete, $request->request->get('_token'))) {
                 $trickRepository->remove($trick);
             }
         }
