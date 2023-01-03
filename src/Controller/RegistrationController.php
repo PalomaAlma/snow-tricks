230dd1c9c5d2aa1cf1ee9f67787f35f41d3fb525
PalomaAlma <palomavacheron@gmail.com>
2023-01-03 23:24:55 +0100

    Some fixes

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -26,7 +26,7 @@ class RegistrationController extends AbstractController
     }
 
     /**
-     * @Route("/register", name="app_register", methods="POST")
+     * @Route("/register", name="app_register", methods={"GET", "POST"})
      */
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
@@ -68,7 +68,7 @@ class RegistrationController extends AbstractController
     }
 
     /**
-     * @Route("/verify/email", name="app_verify_email", methods={"GET"})
+     * @Route("/verify/email", name="app_verify_email", methods={"GET", "POST"})
      */
     public function verifyUserEmail(Request $request, TranslatorInterface $translator): Response
     {
